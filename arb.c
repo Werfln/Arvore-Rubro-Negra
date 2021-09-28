7d385a7725c94ee01250d7fc6162c4c15cc3d53b
Werfln <56362569+Werfln@users.noreply.github.com>
2021-09-28 14:40:30 -0300

    Update arb.c

--- arb.c
+++ arb.c
@@ -10,28 +10,6 @@ int main()
     int nR = 10;
     int aux = 0;
     int *chaves = (int *) malloc(sizeof(int) * (nA));
-
-    //Geração de n números para adicionar à arvore rubro-negra
-    for (int i = 0; i < nA; i++)
-    {
-        chaves[i] = i+1;
-        InserirRN(T,chaves[i]);
-        printf("\nArvore apos a adicao:");
-        preOrdem(T);
-    }
-
-
-    printf("\n--------------------------------------->REMOVER<---------------------------------------\n");
-
-    //Geração de n números de chaves para remover da arvore rubro-negra
-    for (int i = 0; i < nR; i++)
-    {
-        chaves[i] = aux;
-        remover(T,aux);
-        printf("Arvore apos a remocao");
-        preOrdem(T);
-    }
-
     free(chaves);
     return 0;
 }
